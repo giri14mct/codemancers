65d788e2050f71feae05b8d4ca61bfadedbe6cea
Giritharan <70332966+giri14mct@users.noreply.github.com>
2025-02-18 11:26:41 +0530

    Chore: Title for the sit has been updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,8 +38,8 @@ function Clients() {
     <div className="mt-24 rounded-4xl bg-neutral-950 py-20 sm:mt-32 sm:py-32 lg:mt-56">
       <Container>
         <FadeIn className="flex items-center gap-x-8">
-          <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
-            We’ve worked with hundreds of amazing people
+          <h2 className="text-left font-display text-sm font-semibold tracking-wider text-white sm:text-center">
+            Join a growing list of businesses that trust Codemancers
           </h2>
           <div className="h-px flex-auto bg-neutral-800" />
         </FadeIn>
