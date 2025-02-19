270bb222c1055bf526bcf3df89521865e3027900
Giritharan <70332966+giri14mct@users.noreply.github.com>
2025-02-19 15:18:35 +0530

    Feat: Add gif for about us page

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,50 +1,56 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image'
+import team from '@/images/about_us/team.gif'
+import codeReview from '@/images/about_us/code-review.gif'
+import responsive from '@/images/about_us/responsive.gif'
+import checklist from '@/images/about_us/checklist.gif'
+import process from '@/images/about_us/process.gif'
+
 interface Feature {
-  icon: string;
-  title: string;
-  description: string;
+  icon: StaticImageData
+  title: string
+  description: string
 }
 
 const features: Feature[] = [
   {
-    icon: '/images/about_us/team.gif',
+    icon: team,
     title: '12+ years average experience',
     description:
       'We hire only the smartest developers with backgrounds in diverse domains. Having Codemancers working on your side is having decades of experience working to your advantage.',
   },
   {
-    icon: '/images/about_us/code-review.gif',
+    icon: codeReview,
     title: 'Code reviews',
     description:
       'Code reviews are a big part of mentoring at Codemancers. Every line of code gets double-checked for readability and to stay clear from code smells.',
   },
   {
-    icon: '/images/about_us/responsive.gif',
+    icon: responsive,
     title: '100% test coverage',
     description:
       'All of our code has automated tests that run for every single code change on CI servers.',
   },
   {
-    icon: '/images/about_us/checklist.gif',
+    icon: checklist,
     title: 'Solid track record',
     description:
       'Codemancers is well known in the community for our contributions to several widely used open source projects.',
   },
   {
-    icon: '/images/about_us/process.gif',
+    icon: process,
     title: 'Proficient with Agile & Kanban',
     description:
       'With the right mix of processes like sprint planning, daily standups, demos, and retros, we bring order to product development which otherwise can be a chaotic mess.',
   },
-];
+]
 
 const FeatureCard = () => {
   return (
-    <div className="flex flex-wrap justify-center gap-8 mt-16">
+    <div className="mt-16 flex flex-wrap justify-center gap-8">
       {features.map((feature, index) => (
         <div
           key={index}
-          className="flex flex-col w-full max-w-sm gap-8 p-8 text-center border rounded-2xl bg-gradient-to-br from-white to-gray-10"
+          className="to-gray-10 flex w-full max-w-sm flex-col gap-8 rounded-2xl border bg-gradient-to-br from-white p-8 text-center"
         >
           <div className="flex items-center justify-center text-3xl">
             <Image
@@ -55,7 +61,7 @@ const FeatureCard = () => {
             />
           </div>
           <div className="flex flex-col gap-4">
-            <h3 className="text-xl font-semibold text-custom-gray">
+            <h3 className="text-custom-gray text-xl font-semibold">
               {feature.title}
             </h3>
             <p className="text-gray-70">{feature.description}</p>
@@ -63,7 +69,7 @@ const FeatureCard = () => {
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default FeatureCard;
+export default FeatureCard
